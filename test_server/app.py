8761bdaf9ddc34b0d42e9ee2ee439404a1411a3b
ilija-jankovic <ilijajankovic321@gmail.com>
2023-10-29 21:35:21 +1300

    Added multiple vulnerable endpoints

--- test_server/app.py
+++ test_server/app.py
@@ -14,9 +14,26 @@ app = Flask(__name__)
 def home():
     return "This is a vulnerable test website based on the StackOverflow 2010 public data export dataset."
 
-@app.route('/comments')
-def get_comments():
+@app.route('/comments_all_columns')
+def get_comments_all_columns():
     score = request.args['score']
+
+    comments = cursor.execute(f'SELECT TOP 500 * FROM Comments WHERE score = {score}').fetchall()
+
+    return jsonify([str(comment) for comment in comments])
+
+@app.route('/comments_all_columns_join_user')
+def get_comments_all_columns_join_user():
+    score = request.args['score']
+
     comments = cursor.execute(f'SELECT TOP 500 * FROM Comments INNER JOIN Users ON Comments.UserId = Users.Id WHERE Comments.score = {score}').fetchall()
+
+    return jsonify([str(comment) for comment in comments])
+
+@app.route('/comments_single_column')
+def get_comments_single_column():
+    score = request.args['score']
+
+    comments = cursor.execute(f'SELECT TOP 500 text FROM Comments WHERE score = {score}').fetchall()
 
     return jsonify([str(comment) for comment in comments])

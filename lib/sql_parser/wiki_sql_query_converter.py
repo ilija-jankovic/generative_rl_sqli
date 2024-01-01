22760cbb3701fba3315b74f5383766d06d2b2c50
ilija-jankovic <ilijajankovic321@gmail.com>
2024-01-01 16:00:09 +1300

    Deleted unused file

--- lib/sql_parser/wiki_sql_query_converter.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-class WikiSQLQueryConverter:
-    wikisql_queries: List[str]
-
-    def __init__(self, wikisql_queries: List[str]):
-        self.wikisql_queries = wikisql_queries

909891d2ab3c958b0cb70d73d59398cf04622ed1
ilija-jankovic <ilijajankovic321@gmail.com>
2024-08-15 13:19:08 +1200

    Added environment tests for multi-token returns and multiple requests

--- test/environment.py
+++ test/environment.py
@@ -9,24 +9,29 @@ from lib.model.payload_builder import PayloadBuilder
 
 ACTION_SIZE = 10
 
+DICTIONARY = [
+    'publicToken',
+    'privateToken',
+    'publicToken2',
+]
+
+dictionary_size = len(DICTIONARY)
+
 class TestEnvironment(unittest.TestCase):
 
     __env: Environment
     __response: Response
 
-    def setUp(self):
-        super().setUp()
+    def __set_response_body(self, body: str):
+        self.__response._content = str.encode(body)
 
+    def setUp(self):
         self.__response = Response()
-        self.__response._content = str.encode('publicToken publicToken2 123')
+        self.__set_response_body('publicToken publicToken2 123')
 
         self.__env = Environment(
             payload_builder=PayloadBuilder(
-                dictionary=[
-                    'publicToken',
-                    'privateToken',
-                    'publicToken2',
-                ],
+                dictionary=DICTIONARY,
                 prefix='',
                 suffix='',
             ),
@@ -38,16 +43,46 @@ class TestEnvironment(unittest.TestCase):
             send_request_callback=lambda _: self.__response
         )
 
-    def test_new_tokens_count(self):
-        self.__response._content = str.encode('privateToken')
-
+    def __perform_dummy_action(self):
         state, _, __ = self.__env.perform_action(
             tf.zeros([ACTION_SIZE,], dtype=tf.int16)
         )
 
+        return state
+
+    def test_one_new_token_count(self):
+        self.__set_response_body('privateToken')
+        state = self.__perform_dummy_action()
+
         self.assertEqual(
             state[0], 1,
             'State counts one new token after private token returned.'
+        )
+
+    def test_three_new_tokens_count(self):
+        self.__set_response_body('privateToken privateToken2 privateToken3')
+        state = self.__perform_dummy_action()
+
+        self.assertEqual(
+            state[0], 3,
+            'State counts three new tokens after three unique private tokens ' +
+            'returned.'
+        )
+
+    def test_three_consecutive_tokens_count(self):
+        self.__set_response_body('privateToken')
+        state = self.__perform_dummy_action()
+
+        self.__set_response_body('privateToken2')
+        state = self.__perform_dummy_action()
+
+        self.__set_response_body('privateToken3')
+        state = self.__perform_dummy_action()
+
+        self.assertEqual(
+            state[0], 3,
+            'State counts three new tokens after three unique private tokens ' +
+            'consecutively returned.'
         )
 
 if __name__ == '__main__':

5151a584f82a012f9302c320568026ff1d17bfc0
ilija-jankovic <ilijajankovic321@gmail.com>
2024-09-10 20:50:57 +1200

    WikiSQL parsing randomly generates unknown table/column name examples

--- lib/sql_parser/wikisql_parser.py
+++ lib/sql_parser/wikisql_parser.py
@@ -10,19 +10,30 @@ class WikiSQLParser:
     def __init__(self, schema: Dict[str, str]):
         self.schema = schema
 
+    def __generate_random_string(self):
+        return ''.join(random.choices(self.__COLUMN_STRING_ALPHABET, k=random.randint(1, 20)))
+
     def __generate_randomised_example(self, query: str):
-        table = random.choice(list(self.schema.keys()))
-        columns = self.schema[table]
+        is_unknown_table = random.randint(0, 1) == 0
+        
+        table = self.__generate_random_string() if is_unknown_table \
+            else random.choice(list(self.schema.keys()))
+            
+        columns = [
+            self.__generate_random_string()
+                for _ in range(random.randint(1, 5))
+        ] if is_unknown_table else self.schema[table]
         
         query = query.replace('[TABLE_NAME]', table)
         
         while '[COLUMN_NAME]' in query:
             column = random.choice(columns)
+                
             query = query.replace('[COLUMN_NAME]', column, 1)
 
         while '[COMPARISON_VALUE]' in query:
             if random.randint(0, 1) == 0:
-                random_str = ''.join(random.choices(self.__COLUMN_STRING_ALPHABET, k=random.randint(1, 20)))
+                random_str = self.__generate_random_string()
             else:
                 random_str = ''.join(random.choices(string.digits, k=random.randint(1, 6)))
             
